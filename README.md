af0d9a9c668f3747532aecd78a1d664475ce531d
Noelle Burke <noelle.burke@drake.edu>
2025-10-09 17:04:59 -0500

    win check works

--- README.md
+++ README.md
@@ -12,58 +12,9 @@ how to move the circles to the right spots
 position for the board relative and circles absolute
 how to program multiple dots at once lines 21-23 were copied from chatgpt and edited to fit the code
 gameboard js to set up event listener
+gameboard.js for placing chips and switching turns
 
 other sources used 
 https://loading.io/animation/text/
 for win animation
 
-https://gudh.github.io/ihover/dist/index.html
-chip animations
-<!-- Top to bottom-->
-<div class="row">
-  <div class="col-sm-6">
- 
-    <!-- normal -->
-    <div class="ih-item circle effect8 top_to_bottom"><a href="#">
-        <div class="img-container">
-          <div class="img"><img src="images/assets/1.jpg" alt="img"></div>
-        </div>
-        <div class="info-container">
-          <div class="info">
-            <h3>Heading here</h3>
-            <p>Description goes here</p>
-          </div>
-        </div></a></div>
-    <!-- end normal -->
- 
-  </div>
-  <div class="col-sm-6">
- 
-    <!-- colored -->
-    <div class="ih-item circle colored effect8 top_to_bottom"><a href="#">
-        <div class="img-container">
-          <div class="img"><img src="images/assets/2.jpg" alt="img"></div>
-        </div>
-        <div class="info-container">
-          <div class="info">
-            <h3>Heading here</h3>
-            <p>Description goes here</p>
-          </div>
-        </div></a></div>
-    <!-- end colored -->
- 
-  </div>
-</div>
-<!-- end Top to bottom-->
-
-claude answer
-const dots = document.querySelectorAll('.dot');
-
-dots.forEach(dot => {
-    dot.addEventListener('click', function(e) {
-        const column = e.target.dataset.column;
-        const row = e.target.dataset.row;
-        
-        console.log(`Clicked: Row ${row}, Column ${column}`);
-    });
-});
--- gameboard.js
+++ gameboard.js
@@ -44,30 +44,42 @@ dots.forEach(dot => {
     //if (!dot.classList.contains('dot')) return; //makes sure it is a dot 
     
     
- 
+   //if game over break out of loop 
+    //else call placeChip turnSwitch and checkWin 
 function connectFourGame(){
-    //if game over break out of loop 
-    //else call placeChip turnSwitch and checkWin
     placeChip();
+    turnSwitch();
+    checkWin();
 }
 
 function placeChip(row,column,dot){
-    console.log(dot.row , dot.column);
+    //console.log(dot.row , dot.column);
+    const columnUsed = parseInt(dot.dataset.column) - 1; //get the column using variable error need to rename
+    let placedRow = null; 
+    for (let rowUsed = spots.length - 1; rowUsed >= 0; rowUsed--) {//find lowest spot
+        if (spots[rowUsed][columnUsed] === "") { 
+            spots[rowUsed][columnUsed] = currentPlayer; 
+            placedRow=rowUsed;
+            break;
+        }
+    }
+    const finalDot = document.querySelector(
+    `.dot[data-row="${placedRow + 1}"][data-column="${columnUsed + 1}"]`
+);
     if (currentPlayer=="Yellow"){
-    dot.classList.add("Y");
-    spots[row][column]="Y";
+    finalDot.classList.add("Y");
+    spots[placedRow][columnUsed]="Y";
     console.log(spots);
     }
     else{
-        dot.classList.add("B");
-        spots[row][column]="B";
+        finalDot.classList.add("B");
+        spots[placedRow][columnUsed]="B";
         console.log(spots);
     }
 turnSwitch();
-
     //check whose turn where they click place color of chip 
     //and add to spots array
-
+checkWin();
 }
 
 function turnSwitch(){
@@ -76,6 +88,56 @@ function turnSwitch(){
 }
 
 
-// function checkWin(){
+function checkWin(){
 // //look over final from last year and adapt to fit this 
-// }
+        for(let i=0;i<6; i++ ){
+            for(let j=0;j<6;j++){
+            if (
+                i <= 2 &&
+                spots[i] !== undefined &&
+                spots[i + 1] !== undefined &&
+                spots[i + 2] !== undefined &&
+                spots[i + 3] !== undefined &&
+                spots[i][j] !== "" && spots[i][j] === spots[i + 1][j] &&
+                spots[i][j] === spots[i + 2][j] && spots[i][j] === spots[i + 3][j]
+            ){
+                console.log("Vertical win!");
+                gameOver = true;
+                return true;
+                }   
+            if (//accross 
+                j<= 2 &&
+                spots[i] &&
+                spots[i][j]!==""&&
+                spots[i][j]===spots[i][j+1]&&
+                spots[i][j]===spots[i][j+2] && spots[i][j]===spots[i][j+3]
+            ){
+                console.log("game over");
+                gameOver=true;
+                return true;
+            }
+            if(//diagonal
+                i>= 3 && j<=2 &&
+                spots[i] && spots[i-1] && spots[i-2] && spots[i-3] && 
+                spots[i][j] !== "" &&
+                spots[i][j]===spots[i-1][j+1] &&
+                spots[i][j]===spots[i-2][j+2] && spots[i][j]===spots[i-3][j+3]
+            ){
+                console.log("game over");
+                gameOver=true;
+                return true;
+            }
+            if( ///other diagonal
+                i<=2 && j<=2 &&
+                spots[i] && spots[i+1] && spots[i+2] && spots[i+3] && spots[i][j] !== ""&&
+                spots[i][j]===spots[i+1][j+1] &&
+                spots[i][j]=== spots[i+2][j+2] && spots[i][j]===spots[i+3][j+3]
+            ){
+                console.log("game over");
+                gameOver=true;
+                return true;
+            }
+        }
+    } 
+    return false;
+}
